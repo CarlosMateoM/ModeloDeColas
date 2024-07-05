85503582cd01d9b0962cce89df98f1c571154464
Javier E Montes <jaxloz2002@gmail.com>
2024-07-05 11:10:21 -0500

    ajuste del footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,23 @@
 <template>
-    <footer class="bg-white text-gray-600 py-8 ">
-      <div class="container mx-auto px-4">
-        <div class="flex flex-col items-center">
-          <h2 class="text-xl font-semibold text-primary mb-4">
-            Teoría de Colas - Objetos Virtuales de Aprendizaje
-          </h2>
-          <p class="text-sm mb-4 text-center">
-            Facultad de ingenieria - Ingenieria de sistemas y telecomunicaciones
-          </p>
-          <p class="text-xs text-gray-400">
-            &copy; {{ new Date().getFullYear() }} Todos los derechos reservados
-          </p>
-        </div>
+  <footer class="bg-white text-gray-600 py-8 ">
+    <div class="container mx-auto px-4">
+      <div class="flex flex-col items-center">
+        <h2 class="text-xl font-semibold text-primary mb-4 text-center">
+          Teoría de Colas - Objetos Virtuales de Aprendizaje
+        </h2>
+        <p class="text-sm mb-4 text-center">
+          Facultad de ingeniería
+        </p>
+        <p class="text-sm mb-4 text-center"> 
+          - Ingeniería de sistemas y telecomunicaciones
+        </p>
+        <p class="text-xs text-gray-400 text-center">
+          &copy; {{ new Date().getFullYear() }} Todos los derechos reservados
+        </p>
       </div>
-    </footer>
-  </template>
-  
-  <script setup>
-  </script>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+</script>

8c558c198433ef385f573253515830c388fd846f
Javier E Montes <jaxloz2002@gmail.com>
2024-07-04 20:16:29 -0500

    componente para las tarjetas

---
+++ src/views/home/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <articleSummary
+        :image="Imagepng"
+        :title="Titulodelarticulo"
+        :description="Descripcion"
+        :link="Linkdelarticulo"
+        />
+    </div>
+</template>
+<script setup>
+import articleSummary from "@/components/articleSummary.vue"
+</script>

9781bea9b2908a15598556cd61a2673a097e927f
CarlosMateoM <carlosmateo484@gmail.com>
2024-07-04 20:54:11 -0500

    archivos innecesarios eliminados

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1 class="font-bold text-4xl" >Home</h1>
+  </div>
 </template>
+<script setup>
+</script>
